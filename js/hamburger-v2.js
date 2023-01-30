767d7e7eb18c2eb0b469afaec5a9b24d48a6cf7c
Matthew Bramer <70378508+bramermatt@users.noreply.github.com>
2023-01-30 10:35:13 -0500

    new

--- js/hamburger-v2.js
+++ js/hamburger-v2.js
@@ -10,16 +10,23 @@ function myFunction() {
         if (e.target.id === "hamburger"){
           document.querySelector('inline-navbar').classList.add('inline-navbar');
           document.getElementById('hamburger').style.visibility ='hidden';
+          // document.getElementById('title').style.marginTop ='0';
         } else {
+          // document.querySelector('topnav-inline').classList.remove('open');
           document.getElementById('inline-navbar').style.visibility ='visible';
+          // document.getElementById('title').style.marginTop ='0';
         }
       });
       
       document.addEventListener("click", function(e) {
         if (e.target.id === "inline-navbar"){
+          // document.querySelector('topnav-inline').classList.add('open');
           document.getElementById('inline-navbar').style.visibility ='hidden';
+          // document.getElementById('title').style.marginTop ='0';
         } else {
+          // document.querySelector('topnav-inline').classList.remove('open');
           document.getElementById('inline-navbar').style.visibility ='visible';
+          // document.getElementById('title').style.marginTop ='0';
         }
       });
   }
