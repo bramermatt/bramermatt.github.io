767e0558c542dcc114c2691d23aca5d761784152
Matthew Bramer <70378508+bramermatt@users.noreply.github.com>
2023-05-17 11:07:07 -0400

    new homepage

--- js/active.js
+++ js/active.js
@@ -1,34 +1,19 @@
-// Smooth scrolling for browsers that don't support CSS smooth scrolling
-if (window.getComputedStyle(document.documentElement).scrollBehavior !== 'smooth') {
-    document.querySelectorAll('a[href^="#"]').forEach(internalLink => {
-        const targetElement = document.querySelector(internalLink.getAttribute('href'));
-        if (targetElement) {
-            internalLink.addEventListener('click', (e) => {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth',
-                });
-                e.preventDefault();
-            });
-        }
-    });
-}
-
 window.addEventListener('DOMContentLoaded', () => {
 
 	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
+	  entries.forEach(entry => {
+		const id = entry.target.getAttribute('id');
+		if (entry.intersectionRatio > 0) {
+		  document.querySelector(`right-nav-bar li a[href="#${id}"]`).parentElement.classList.add('active');
+		} else {
+		  document.querySelector(`right-nav-bar li a[href="#${id}"]`).parentElement.classList.remove('active');
+		}
+	  });
 	});
-
+  
 	// Track all sections that have an `id` applied
 	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
+	  observer.observe(section);
 	});
 	
-});
+  });
