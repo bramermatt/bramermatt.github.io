4447401d57c08602ecd01e6bfeb68d207d30b9dc
Matthew Bramer <70378508+bramermatt@users.noreply.github.com>
2023-06-29 14:38:14 -0400

    new new new new new

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,32 +1,10 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-    var x = document.getElementById("topnav-inline");
+    var x = document.getElementById("nav-bar");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
+      x.style.marginRight = "2%";
     }
-
-    document.addEventListener("touchend", function(e) {
-        if (e.target.id === "hamburger"){
-          document.querySelector('topnav-inline').classList.add('topnav-inline');
-          document.getElementById('hamburger').style.visibility ='hidden';
-          // document.getElementById('title').style.marginTop ='0';
-        } else {
-          // document.querySelector('topnav-inline').classList.remove('open');
-          document.getElementById('topnav-inline').style.visibility ='visible';
-          // document.getElementById('title').style.marginTop ='0';
-        }
-      });
-      
-      document.addEventListener("click", function(e) {
-        if (e.target.id === "topnav-inline"){
-          // document.querySelector('topnav-inline').classList.add('open');
-          document.getElementById('topnav-inline').style.visibility ='hidden';
-          // document.getElementById('title').style.marginTop ='0';
-        } else {
-          // document.querySelector('topnav-inline').classList.remove('open');
-          document.getElementById('topnav-inline').style.visibility ='visible';
-          // document.getElementById('title').style.marginTop ='0';
-        }
-      });
   }
