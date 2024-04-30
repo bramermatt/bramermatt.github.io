c4df9282c315eee85dc9bec12f5dcc9f0e94fe6c
Matthew Bramer <70378508+bramermatt@users.noreply.github.com>
2024-04-30 15:48:25 -0400

    2024.4.30.2

--- dropdown.js
+++ dropdown.js
@@ -1,17 +1,18 @@
-        /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-        function myFunction() {
-            document.getElementById("myDropdown").classList.toggle("show");
-          }
-          
-          // Close the dropdown if the user clicks outside of it
-          window.onclick = function(e) {
-            if (!e.target.matches('.dropbtn')) {
-              var dropdowns = document.getElementsByClassName("dropdown-content");
-              for (var i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                  openDropdown.classList.remove('show');
-                }
-              }
-            }
-          }
+// Function to toggle the dropdown
+function myFunction(dropdownId) {
+    var dropdownContent = document.getElementById(dropdownId);
+    dropdownContent.classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
