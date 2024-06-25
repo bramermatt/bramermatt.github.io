7a04b1078f3d72cf489fc0b4aaa363cfaa0a9622
Matthew Bramer <70378508+bramermatt@users.noreply.github.com>
2024-06-25 16:24:52 -0400

    still working

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 function mobileMenu() {
   const menu = document.getElementById('nav-mobile');
   const icon = document.getElementById('menu-icon');
+  const menuItems = document.querySelectorAll('.nav-mobile li a'); // Select all menu items
   
   if (menu.classList.contains('open')) {
       menu.classList.remove('open');
@@ -11,4 +12,13 @@ function mobileMenu() {
       icon.classList.remove('fa-bars');
       icon.classList.add('fa-times');
   }
+
+  // Close menu when a menu item is clicked
+  menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+          menu.classList.remove('open');
+          icon.classList.remove('fa-times');
+          icon.classList.add('fa-bars');
+      });
+  });
 }
